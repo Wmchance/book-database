617cee4844f4f9fbf0fd0d86505d8292ebd11320
Will <wmchance@gmail.com>
2022-12-20 09:52:57 +0700

    import models to app & initialize  books.db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import models
+from models import Base, Book, session, engine
 # main menu - add, search, analysis, exit, view
 # add books to the db
 # edit books
@@ -6,3 +7,6 @@
 # delete books 
 # data cleaning 
 # loop runs the project
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
