b0cd7c72c536e3a6aa0c5e4bea634750e54d742d
Will <wmchance@gmail.com>
2022-12-20 09:45:46 +0700

    add db setup & Book model

--- models.py
+++ models.py
@@ -1,3 +1,24 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///books.db', echo = False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    published_date = Column(Date)
+    price = Column(Integer)
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
 # create a db
     #books.db
 # create a model
